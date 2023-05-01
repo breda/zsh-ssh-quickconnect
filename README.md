0de3714871549f494cced2f9f1b593362df6cd6d
Reda Bouchaala <bouchaala.reda@gmail.com>
2023-05-01 15:36:08 +0100

    Add script & update README

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # zsh-ssh-quickconnect
+
+Just a simple utility function that grabs all hosts from .ssh/config file & known_hosts (when a Host entry uses glob matching)
+and prints them for selection using fzf and SSHes into the selected entry.
+
+Just clone this repo somewhere then source the `.sh` script in your .zshrc somewhere
+
+```bash
+git clone https://github.com/breda/zsh-ssh-quickconnect $ZSH/plugins/ssh-quick-connect
+```
+
+
+```bash
+source $ZSH/plugins/zsh-ssh-quickconnect.plugin.sh
+```
+
+Then run `sshqc` anytime to start SSHing
+
+```bash
+sshqc
+```
+
+## Why this might be useful for you
+I'm usually managing a couple boxes that I need to SSH into, the way I manage them is I have DNS entries that looks like "bastion.staging.work"
+ or something like that, and then in my SSH config file I have something like
+
+```
+Host *.staging.work
+    IdentityFile /home/user/.ssh/work/staging
+
+Host *.production.work
+    IdentityFile /home/user/.ssh/work/prod
+```
+
+But because I name the DNS entries differently I can't be bothered to remember them all, so this utility function grabs all defined `Host`s from my SSH config,
+expands any entries containing globs with I have on my known_hosts file and displays a nice list to choose from. Making it easier and faster to SSH into a target box.
---
+++ ssh-quickconnect.plugin.sh
@@ -0,0 +1,41 @@
+#
+# SSH quickconnect
+#
+# Just a simple utility function that grabs all hosts from .ssh/config file & known_hosts (when a Host entry uses glob matching)
+# and prints them for selection using fzf and SSHes into the selected entry.
+#
+
+function sshqc() {
+    local ssh_config="$HOME/.ssh/config"
+    local ssh_known_hosts="$HOME/.ssh/known_hosts"
+    local disregard="github.com|gitlab.com|bitbucket.org"
+    
+
+    # Get Host entries from config
+    local hosts=($(grep -E "^Host " $ssh_config | cut -f 2 -d " " | grep -v "^*$" | grep -v -E $disregard))
+    local remotes=()
+
+    for host in $hosts
+    do
+        # Grab the user from SSH config
+        local user=$(ssh -G $host | grep -E '^user ' | cut -f 2 -d " ")
+
+        # In this case, we have a glob in the Host name
+        # We get all matching hostnames by looking them up in the known_hosts file
+        if [[ $host =~ "\*" ]]; then
+            known_hosts=($(grep -E "^$host" $ssh_known_hosts 2>/dev/null | cut -f 1 -d " " | uniq))
+
+            for known_host in $known_hosts
+            do
+                remotes=(${remotes[@]} "$user@$known_host")
+            done
+        else
+            # Append the entry to remotes
+            remotes=(${remotes[@]} "$user@$host")
+        fi
+    done
+
+    selected=$(printf "%s\n" "${remotes[@]}" | fzf)
+    echo "SSHing into $selected now... \n"
+    eval "ssh $selected"
+}
